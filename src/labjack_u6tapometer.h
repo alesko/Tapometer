c8c915dc325580f05e236fee2875bbbc0e696788
alesko <alexander.skoglund@gmail.com>
2010-12-08 18:05:12 +0100

    new file:   AUTHORS 	new file:   ChangeLog 	modified:   README 	new file:   TapoMeter 	new file:   src/Makefile 	new file:   src/Makefile.Linux 	new file:   src/Makefile.MacOS 	new file:   src/Makefile.am~ 	new file:   src/Makefile.in~ 	new file:   src/Makefile~ 	new file:   src/labjack_u6tapometer.cpp 	new file:   src/labjack_u6tapometer.cpp~ 	new file:   src/labjack_u6tapometer.h 	new file:   src/labjack_u6tapometer.h~ 	new file:   src/labjack_u6tapometer.o 	new file:   src/labjackusb.h 	new file:   src/main.cpp 	new file:   src/main.cpp~ 	new file:   src/main.o 	new file:   src/u6.c 	new file:   src/u6.h 	new file:   src/u6.o

---
+++ src/labjack_u6tapometer.h
@@ -0,0 +1,137 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Alexander Skoglund, Karolinska Institute
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Karolinska Institute nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+#ifndef LABJACK_u6TAPOMETER_H_
+#define LABJACK_u6TAPOMETER_H_
+
+
+#include "u6.h"
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+#include <fstream>
+#include <math.h>
+#include <sys/time.h>
+#include <iomanip>
+
+/* OpenGL includes */
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h> //#include <glut/glut.h> 
+#endif
+
+
+
+class TapoMeter{
+ protected:
+  static TapoMeter *instance_; // To make callback functions work
+
+ public:
+
+  TapoMeter(int w, int h);
+  ~TapoMeter();
+
+  int Init(void);
+  void close_LJ_device(void);
+  void SampleData(void);
+
+  //double GetRunTime(void);
+  int  DrawCircle(double pos_x, double pos_y, double radius, double color[3]);
+  int  DrawPlot(std::vector<double> data,double color[3], double t1, double t2);
+  void DoRedraw(int flag, std::vector<double> d,double t1, double t2);
+
+  double GetRunTime(void);
+  double get_tstart(void);
+
+  bool set_is_running(bool state);
+  bool get_is_running(void);
+  int get_screen_width(void);
+  int get_screen_height(void);
+  int set_screen_width(int w);
+  int set_screen_height(int h);
+  int set_fps(int fps);
+  int get_fps(void);
+
+  void increase_k_(void);
+  void increase_c_(void);
+  void decrease_k_(void);
+  void decrease_c_(void);
+  
+ private:
+
+  int screen_width_;       // Screen dimensions       
+  int screen_height_;      
+  bool is_running_;
+  int fps_;
+  double tstart_;
+
+  // Screen handling
+      
+  // Data storange
+  std::ofstream data_file_;
+  std::vector<double> data_;
+
+  // Counters
+  long loops_;
+  int flag_;
+  int counts_;
+
+  // Tap detection algorithm paramters
+  float k_; // Multiplier
+  float c_; // Threshold
+  float dt_c_;
+  float dt_k_;
+  float K_MAX;
+  float C_MAX;
+
+  // LabJack 
+  HANDLE            h_device_;
+  u6CalibrationInfo cali_info_;
+  int local_ID_;
+  long error_;
+
+};
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,149 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Alexander Skoglund, Karolinska Institute
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Karolinska Institute nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+
+#include "labjack_u6tapometer.h"
+
+TapoMeter *tap_counter;  // Global variable 
+
+
+using namespace std;
+
+void HandleKeyboard(unsigned char key,int x, int y)
+{
+  switch (key) {
+  case '<':                           // Decrease multiplier
+  case ',':
+    tap_counter->decrease_k_();
+    break;
+  case '>':                           // Increase multiplier
+  case '.':
+    tap_counter->increase_k_();
+    break;
+  case '-':                           // Decrease threshold 
+  case '_':
+    tap_counter->decrease_c_();
+    break;
+  case '+':                           // Increase threshold 
+  case '=':
+    tap_counter->increase_c_();
+    break;    
+  case 27:                            // Quit
+  case 'Q':
+  case 'q':
+    {
+      tap_counter->close_LJ_device();
+      usleep(1000);
+      tap_counter->set_is_running(false);      
+      break;
+    }
+  }
+}
+
+
+void HandleIdle(void)
+{
+   static double tstart = -1;
+   double tstop;
+
+   double fps = (double)tap_counter->get_fps();
+   if (tstart < 0)
+      tstart = tap_counter->GetRunTime();
+   tstop = tap_counter->GetRunTime();
+   if (tstop - tstart > 1.0/fps) {
+      glutPostRedisplay();
+      tstart = tstop;
+   }
+}
+
+void HandleReshape(int w,int h)
+{
+  tap_counter->set_screen_width(w);
+  tap_counter->set_screen_height(h);
+}
+
+void IsRunning(void)
+{
+  
+  if(tap_counter->get_is_running())
+    tap_counter->SampleData();
+  else
+    exit(0);
+
+}
+
+
+
+int main(int argc, char* argv[])
+{
+
+  // Initialize the LabJack device
+  tap_counter = new TapoMeter(640,480);
+  tap_counter->set_is_running(true);
+  tap_counter->set_fps(200.0);
+
+/* You must call glutInit before any other OpenGL/GLUT calls */
+  glutInit(&argc,argv); 
+  //glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // | GLUT_DEPTH);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // | GLUT_DEPTH);
+  glutCreateWindow("Tapometer");
+  //glutInitWindowSize(500,500);
+  glutReshapeWindow(tap_counter->get_screen_width(),tap_counter->get_screen_height());
+  glutInitWindowPosition(0,0); 
+  glutReshapeFunc(HandleReshape);
+  glutKeyboardFunc(HandleKeyboard);
+  glutIdleFunc(HandleIdle);
+
+  //glutDisplayFunc(display);
+  glutDisplayFunc(IsRunning);
+  //init();
+  glutMainLoop();
+
+  // Set up GLUT environment
+	/* glutInit(&argc,argv);
+  
+  glutReshapeWindow(tap_counter->get_screen_width(),tap_counter->get_screen_height());
+  glutInitWindowPosition(0,0);
+  glutReshapeFunc(HandleReshape);
+  glutKeyboardFunc(HandleKeyboard);
+  //glutIdleFunc(HandleIdle);
+  glutDisplayFunc(IsRunning);
+  glutMainLoop();*/
+
+  return(0);
+
+}
+
